6d732aea066355868cea164b9eb5eaf4572a4518
Brodie Davis <bdavis9d@gmail.com>
2017-09-03 12:31:42 -0400

    create Manuscript global JS object

--- resources/static/js/manuscript.js
+++ resources/static/js/manuscript.js
@@ -1,18 +1,36 @@
-Manuscript = {};
+var Manuscript;
 
-Manuscript.root = '/'
+(function () {
+    if(Manuscript === undefined){
+        Manuscript = {};
+    }
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-Manuscript.openNav = function() {
+  // Return root - full path if electron, '/' if web
+  Object.defineProperty(Manuscript, 'root', {
+    get: function() {
+      try {
+        return __dirname
+      } catch (e) {
+        return '/'
+      }
+    },
+    set: function () {
+      throw "read only property"
+    }
+  });
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  Manuscript.openNav = function() {
     document.getElementById("mySidenav").style.width = "200px";
     // document.getElementById("main").style.marginLeft = "200px";
-}
+  }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-Manuscript.closeNav = function() {
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  Manuscript.closeNav = function() {
     document.getElementById("mySidenav").style.width = "0";
     // document.getElementById("main").style.marginLeft = "0";
-}
-Manuscript.showItem = function(item_name){
-    
-}
+  }
+  Manuscript.showItem = function(item_name){
+
+  }
+})();
