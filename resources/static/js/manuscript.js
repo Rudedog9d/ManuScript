5a96b89440f13484b53751e263f04e33a5f6e3ea
Brodie Davis <bdavis9d@gmail.com>
2017-09-11 21:54:16 -0400

    edit button in post-list now works; refactor tabs some

--- resources/static/js/manuscript.js
+++ resources/static/js/manuscript.js
@@ -127,18 +127,27 @@ var Manuscript;
     return ret;
   };
 
-  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-  Manuscript.openNav = function () {
-    document.getElementById("mySidenav").style.width = "200px";
-    // document.getElementById("main").style.marginLeft = "200px";
-  }
+  Manuscript.db.findOne = function (query, options) {
+    return Manuscript.db.find(query, options)[0];
+  };
 
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  Manuscript.closeNav = function () {
-    document.getElementById("mySidenav").style.width = "0";
-    // document.getElementById("main").style.marginLeft = "0";
-  }
-  Manuscript.showItem = function (item_name) {
+  Manuscript.tags = {};
 
-  }
+  /**
+   * Update all tags registered with Manuscript
+   * @param options - Options to pass to each tag
+   */
+  Manuscript.updateTags = function (options) {
+    //todo: use this
+    for(let tag in Manuscript.tags){
+      Manuscript.tags[tag].update(options);
+    }
+  };
+
+  Manuscript.showTab = function (tabId) {
+    $('ul.tabs').tabs('select_tab', 'tab-' + tabId);
+
+    // Store the user's last select tab
+    window.localStorage.setItem('tab', tabId);
+  };
 })();
